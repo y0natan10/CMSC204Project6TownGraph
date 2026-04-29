a2f9a41255fcc72dfd6d98c64e18161f220cafa5
Yonatan Rubin <yonirubin2002@gmail.com>
2026-04-29 14:26:34 -0400

    Mostly Finished Code
    finished and tested most of the methods in TownGraphManager, something
    is wrong with the getPath method, but other than that, the base methods
    all work. still need to add a method to read from a file

--- src/main/java/TownGraphManager.java
+++ src/main/java/TownGraphManager.java
@@ -2,67 +2,84 @@
 //M21105076
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TownGraphManager implements TownGraphManagerInterface {
 
+	private Graph townGraph = new Graph();
+
 	@Override
 	public boolean addRoad(String town1, String town2, int weight, String roadName) {
-		// TODO Auto-generated method stub
-		return false;
+		Town source = new Town(town1);
+		Town destination = new Town(town2);
+
+		// addEdge handles what happens if either town1 or town2 already exists
+		return (this.townGraph.addEdge(source, destination, weight, roadName) != null);
 	}
 
 	@Override
 	public String getRoad(String town1, String town2) {
-		// TODO Auto-generated method stub
-		return null;
+		Town source = new Town(town1);
+		Town destination = new Town(town2);
+		return this.townGraph.getEdge(source, destination).getDescription();
 	}
 
 	@Override
 	public boolean addTown(String v) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.townGraph.addVertex(new Town(v));
 	}
 
 	@Override
 	public boolean containsTown(String v) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.townGraph.containsVertex(new Town(v));
 	}
 
 	@Override
 	public boolean containsRoadConnection(String town1, String town2) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.townGraph.containsEdge(new Town(town1), new Town(town2));
 	}
 
 	@Override
 	public ArrayList<String> allRoads() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> res = new ArrayList<String>();
+		for (Road r : this.townGraph.getRoads()) {
+			res.add(r.getDescription());
+		}
+		Collections.sort(res);
+		return res;
 	}
 
 	@Override
 	public boolean deleteRoadConnection(String town1, String town2, String road) {
-		// TODO Auto-generated method stub
-		return false;
+		return (this.townGraph.removeEdge(new Town(town1), new Town(town2), 1, road) != null);
 	}
 
 	@Override
 	public boolean deleteTown(String v) {
-		// TODO Auto-generated method stub
-		return false;
+		return (this.townGraph.removeVertex(new Town(v)));
 	}
 
 	@Override
 	public ArrayList<String> allTowns() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> res = new ArrayList<String>();
+		for (Town r : this.townGraph.getTowns()) {
+			res.add(r.getName());
+		}
+		Collections.sort(res);
+		return res;
 	}
 
 	@Override
 	public ArrayList<String> getPath(String town1, String town2) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> res = new ArrayList<String>();
+		this.townGraph.dijkstraShortestPath(new Town(town1));
+		Town goal = new Town(town1);
+		Town current = new Town(town2);
+		while (current.getBackPath() != null && !current.getBackPath().equals(goal)) {
+			res.addFirst(current.getName());
+			current = current.getBackPath();
+		}
+		return res;
 	}
 
 }
